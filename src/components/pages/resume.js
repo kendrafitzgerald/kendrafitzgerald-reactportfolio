ca8b656068a8004eead537f187727f0c99787f78
Kendra Fitzgerald <kendrajfitzgerald@gmail.com>
2023-05-23 21:31:56 -0400

    changed resume section

--- src/components/pages/resume.js
+++ src/components/pages/resume.js
@@ -1,12 +1,12 @@
 /* eslint-disable no-unused-vars */
 import React, {useState} from 'react';
-import {Document, Page} from 'react-pdf';
+// import {Document, Page} from 'react-pdf';
 import resumePDF from '../../utils/Kendra-Fitzgerald-Resume.pdf';
 import { pdfjs } from 'react-pdf';
 import 'react-pdf/dist/esm/Page/TextLayer.css';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 import {faDownload} from '@fortawesome/free-solid-svg-icons'
-import {faGithub, faLinkedin, faTwitter} from '@fortawesome/free-brands-svg-icons'
+// import {faGithub, faLinkedin, faTwitter} from '@fortawesome/free-brands-svg-icons'
 //function necessary to render PDF doc on screen
 pdfjs.GlobalWorkerOptions.workerSrc = new URL(
   'pdfjs-dist/build/pdf.worker.min.js',
@@ -16,7 +16,7 @@ pdfjs.GlobalWorkerOptions.workerSrc = new URL(
 const styles = {
     body: {
       background: "#000018",
-      height: 525,
+      height: 600,
       width: 800,
       marginLeft: 'auto',
       marginRight: 'auto'
@@ -26,26 +26,43 @@ const styles = {
       fontFamily: 'Rockwell, Courier Bold, Courier'
     },
 
-  footer: {
-          padding: '10px',
-          textAlign: 'center',
-          marginTop: 300
+  // footer: {
+  //         padding: '10px',
+  //         textAlign: 'center',
+  //         marginTop: 300
       
-      },
+  //     },
       icon: {
           color: 'white',
           fontSize: '30px',
           padding: 5
       },
+      list: {
+        color: 'white',
+        fontFamily: 'Rockwell, Courier Bold, Courier',
+        fontSize: 20
+      },
+      titles: {
+        color: 'white',
+        fontFamily: 'Rockwell, Courier Bold, Courier',
+        fontSize: 30,
+        paddingBottom: 10,
+        listStyleType: 'none'
+      },
+      prof: {
+        color: 'white',
+        fontFamily: 'Rockwell, Courier Bold, Courier',
+        fontSize: 35,
+      }
   };
 export default function Resume(){
-  //useState variables needed for PDF rendering
-  const [numPages, setNumPages] = useState(null);
-  const [pageNumber, setPageNumber] = useState(1);
+ //useState variables needed for PDF rendering
+//   const [numPages, setNumPages] = useState(null);
+//   const [pageNumber, setPageNumber] = useState(1);
 //function needed to render PDF
-  function onDocumentLoadSuccess({ numPages }) {
-    setNumPages(numPages);
-  }
+//   function onDocumentLoadSuccess({ numPages }) {
+//     setNumPages(numPages);
+//   }
     return(
       //renders PDF doc and allows for download
       <div>
@@ -53,12 +70,33 @@ export default function Resume(){
             <h1 style={styles.header}>Resume</h1>
             <a href={resumePDF} download ="kendrafitzgeraldResume.pdf"style={styles.header}
             >Click Here to Download My Resume <FontAwesomeIcon icon={faDownload}/></a>
+            <h2 style={styles.prof}>Proficiences</h2>
+            <ul>
+              <li style={styles.titles}>
+                Front-End
+              </li>
+              <li style={styles.header}>HTML5</li>
+              <li style={styles.header}>CSS3</li>
+              <li style={styles.header}>JavaScript</li>
+              <li style={styles.header}>jQuery</li>
+              <li style={styles.header}>React</li>
+              <li style={styles.header} >Bootstrap</li>
+              <li style={styles.header}>Bulma</li>
+              <li style={styles.header}>Responsive Design</li>
+              </ul>
+              <br></br>
+              <ul>
+              <li style={styles.titles}>
+                Back-End
+              </li>
+              <li style={styles.header}>APIs</li>
+              <li style={styles.header}>Node</li>
+              <li style={styles.header}>Express</li>
+              <li style={styles.header}>MySql, Sequelize</li>
+              <li style={styles.header}>MongoDB, Mongoose</li>
+              <li style={styles.header} >GraphQl</li>
+              </ul>
         </div>
-         <footer style={styles.footer}>
-         <a style= {styles.icon} href='https://github.com/kendrafitzgerald'><FontAwesomeIcon icon={faGithub}/></a>
-         <a style= {styles.icon} href="https://www.linkedin.com/in/kendra-fitzgerald-b73780175/"><FontAwesomeIcon icon={faLinkedin}/></a>
-         <a style= {styles.icon} href='https://twitter.com/kendrajoyy'><FontAwesomeIcon icon={faTwitter}/></a>
-         </footer>
          </div>
     );
 }
